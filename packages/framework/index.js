4b191579e41312bda0359a486c658a8383543fba
Lukas Schardt <lukas@buerozimmer.de>
2020-12-27 23:16:10 +0100

    Readded indexjs

---
+++ packages/framework/index.js
@@ -0,0 +1,14 @@
+import './bootstrap.js';
+
+import express from 'express';
+import {routes} from "./router/routes.js";
+const app = express();
+const port = 3001;
+
+app.use(express.static('.build/public'));
+
+routes({router: app});
+
+app.listen(port, () => {
+    console.log(`element-js server listening at: http://localhost:${port}`)
+})
