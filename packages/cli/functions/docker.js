0501d7106ec30221cba550a06067f56c2cda737f
Lukas Schardt <lukas@buerozimmer.de>
2021-01-07 18:19:38 +0100

    Move prebuild method to build tasks

--- packages/cli/functions/docker.js
+++ packages/cli/functions/docker.js
@@ -1,22 +1,6 @@
-import {registerAvailableComponents} from "../../framework/loaders/component-loader";
-import {loadSettings} from "../../framework/config";
 import path from "path";
 import fs from "fs";
 
-const prepareForDocker = async () => {
-    const settings = await loadSettings();
-
-    const availableComponents = (await registerAvailableComponents({generateCssBundles: true}));
-
-    const generatedDirectory = path.join(settings.buildDirectory, "generated");
-    if (!fs.existsSync(generatedDirectory)) {
-        fs.mkdirSync(generatedDirectory);
-    }
-
-    const manifest = {availableComponents};
-
-    fs.writeFileSync(path.join(generatedDirectory, "manifest.json"), JSON.stringify(manifest), {encoding: "utf-8"});
-};
 
 const publishDockerFile = async () => {
     fs.copyFileSync(path.resolve(moon.currentDirectory, "packages/cli/docker/Dockerfile"), path.join(moon.currentWorkingDirectory, "Dockerfile"));
@@ -24,4 +8,4 @@ const publishDockerFile = async () => {
 };
 
 
-export { publishDockerFile, prepareForDocker };
+export { publishDockerFile };
