d55f24026f765fe20893568140f1f4f08df5f9cd
Lukas Schardt <lukas@buerozimmer.de>
2021-01-07 17:28:01 +0100

    Added support for serverless, temporarily removed lib from gitignore

---
+++ babel.config.bundle.js
@@ -0,0 +1,16 @@
+module.exports = (api) => {
+    api.cache(false);
+
+    return {
+        presets: [["@babel/preset-env", { "targets": { "node": 12 } }]],
+        plugins: [
+            "@babel/plugin-proposal-nullish-coalescing-operator",
+            "@babel/plugin-proposal-optional-chaining",
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-proposal-export-default-from",
+            "module-resolver"
+        ],
+
+        sourceMaps: false
+    }
+}
---
+++ packages/serverless/entry.js
@@ -0,0 +1,10 @@
+require = require("esm")(module/*, options*/)
+
+const { prepareServer } = require("../framework/index.js");
+const serverless = require("serverless-http");
+
+module.exports.handler = async (event, context) => {
+    const app = await prepareServer();
+
+    return serverless(app)(event, context);
+};
