77a0d2c74dc847654139cac44927da7fb8f8e9e6
Lukas Schardt <lukas@buerozimmer.de>
2021-01-07 17:49:32 +0100

    Readded prepare task to avoid creating bundle on serverless environments.

--- packages/cli/functions/docker.js
+++ packages/cli/functions/docker.js
@@ -4,20 +4,20 @@ const exec = require('child_process').exec;
 const path = require("path");
 const fs = require("fs");
 
-// const prepareForDocker = async () => {
-//     const settings = await loadSettings();
-//
-//     const availableComponents = (await registerAvailableComponents({generateCssBundles: true}));
-//
-//     const generatedDirectory = path.join(settings.buildDirectory, "generated");
-//     if (!fs.existsSync(generatedDirectory)) {
-//         fs.mkdirSync(generatedDirectory);
-//     }
-//
-//     const manifest = {availableComponents};
-//
-//     fs.writeFileSync(path.join(generatedDirectory, "manifest.json"), JSON.stringify(manifest), {encoding: "utf-8"});
-// };
+const prepareForDocker = async () => {
+    const settings = await loadSettings();
+
+    const availableComponents = (await registerAvailableComponents({generateCssBundles: true}));
+
+    const generatedDirectory = path.join(settings.buildDirectory, "generated");
+    if (!fs.existsSync(generatedDirectory)) {
+        fs.mkdirSync(generatedDirectory);
+    }
+
+    const manifest = {availableComponents};
+
+    fs.writeFileSync(path.join(generatedDirectory, "manifest.json"), JSON.stringify(manifest), {encoding: "utf-8"});
+};
 
 const publishDockerFile = async () => {
     fs.copyFileSync(path.resolve(moon.currentDirectory, "packages/cli/docker/Dockerfile"), path.join(moon.currentWorkingDirectory, "Dockerfile"));
@@ -25,4 +25,4 @@ const publishDockerFile = async () => {
 };
 
 
-export { publishDockerFile };
+export { publishDockerFile, prepareForDocker };
