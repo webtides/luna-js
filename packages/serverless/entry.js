cd9bbbd0891bb2320b53252f06c2aa97b9748ead
Lukas Schardt <lukas@buerozimmer.de>
2021-01-09 21:44:00 +0100

    Removed rollup-postcss-plugin

--- packages/serverless/entry.js
+++ packages/serverless/entry.js
@@ -1,6 +1,5 @@
 const { prepareServer } = require("../framework/index.js");
 const serverless = require("serverless-http");
-const path = require("path");
 
 module.exports.handler = async (event, context) => {
     const app = await prepareServer();
