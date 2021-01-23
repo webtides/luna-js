c5a58638be43d2d0b6966ee2d93b7662d683c9f0
Lukas Schardt <lukas@buerozimmer.de>
2021-01-23 15:32:30 +0100

    Removed obsolete esm entries

--- packages/cli/entry.js
+++
@@ -1,3 +0,0 @@
-require = require("esm")(module/*, options*/)
-
-module.exports = require("./index.js")
--- packages/framework/entry.js
+++
@@ -1,3 +0,0 @@
-require = require("esm")(module/*, options*/)
-
-require("./index.js").startServer();
