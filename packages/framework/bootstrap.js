378b740bd49344520a17e10ce06170d11d8d57e5
Lukas Schardt <lukas@buerozimmer.de>
2021-01-28 13:45:12 +0100

    Added dotenv for automatic env-file loading.

--- packages/framework/bootstrap.js
+++ packages/framework/bootstrap.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import {callHook} from "./hooks";
 import * as litHtml from "@popeindustries/lit-html-server";
 import { unsafeHTML} from "@popeindustries/lit-html-server/directives/unsafe-html";
