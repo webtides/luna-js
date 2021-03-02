f3b9d91e7d9bc9193e082fcb53793e5dd83db5a7
Lukas Schardt <lukas@buerozimmer.de>
2021-03-02 09:00:02 +0100

    #6: Use only the start server script on root

--- start.js
+++ start.js
@@ -1,1 +1,6 @@
-require("./lib/packages/framework/framework");
+/**
+ * Imports the startServer method from the build folders. Only works
+ * after moon-js has been transpiled.
+ */
+const { startServer } = require("./lib/packages/framework");
+startServer();
