2e2106e9f43c3b3559df0e9a05c0658fe6cd62b2
Lukas Schardt <lukas@buerozimmer.de>
2021-03-09 16:22:01 +0100

    #17,#18: allow a configurable api and asset path. Pass these configs to the client

---
+++ packages/client/functions/moon.js
@@ -0,0 +1,5 @@
+import moonObject from "../../framework/shared/moon-object";
+
+(function() {
+    window.moon = moonObject(window.moonConfig);
+})();
--- packages/framework/moon.js
+++ packages/framework/moon.js
@@ -1,22 +1,37 @@
-import {loadSettings} from "./config";
+import {getSerializableConfig, loadSettings} from "./config";
+import {loadHooks} from "./loaders/hooks-loader";
+import {callHook} from "./hooks";
+import {HOOKS} from "./hooks/definitions";
 
-const initializeMoonObject = async () => {
-    const settings = await loadSettings();
+import moonObject from "./shared/moon-object";
 
-    global.moon = {
-        /**
-         * Applies the configured asset path to this resource.
-         *
-         * @param path          The path of the resource without any configured
-         *                      asset context.
-         *
-         * @returns {string}
-         */
-        asset: (path) => {
-            const assetContextPath = settings.assets?.context ?? '';
-            return `${assetContextPath}${path}`;
-        }
-    };
+/**
+ * The main initialization method of our moon js framework.
+ * Here we load all settings, and initialize all required elements
+ * on startup.
+ *
+ * Does not handle component/route registration.
+ *
+ * @returns {Promise<boolean>}
+ */
+const initializeMoon = async () => {
+    // First we load all settings.
+    if (!(await loadSettings())) {
+        return false;
+    }
+
+    // Load and register all available hooks.
+    await loadHooks();
+
+    const config = getSerializableConfig();
+
+    initializeMoonObject(config);
+    return true;
 };
 
-export { initializeMoonObject };
+
+const initializeMoonObject = (config) => {
+    global.moon = callHook(HOOKS.MOON_INITIALIZE, moonObject(config));
+};
+
+export { initializeMoon };
