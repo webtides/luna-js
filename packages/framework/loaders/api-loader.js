a4758c6342fa9cc6003d1f29ccab7f13981644c5
Lukas Schardt <lukas@buerozimmer.de>
2021-03-18 23:11:51 +0100

    Add comments for the loaders and configuration files.

--- packages/framework/loaders/api-loader.js
+++ packages/framework/loaders/api-loader.js
@@ -1,6 +1,17 @@
 import {loadManifest, loadSettings} from "../config";
 import path from "path";
 
+/**
+ * Loads all available api routes from the generated manifest and registers
+ * these hooks.
+ *
+ * @returns {Promise<{
+ *  file: string,
+ *  relativePath: string,
+ *  route: string,
+ *  name: string
+ * }[]>}
+ */
 const loadApis = async () => {
     const settings = await loadSettings();
 
--- packages/framework/loaders/pages-loader.js
+++ packages/framework/loaders/pages-loader.js
@@ -1,19 +1,41 @@
 import path from "path";
-import {loadManifest, loadSettings} from '../config.js';
 
 import {html, renderToString} from "@popeindustries/lit-html-server";
+import {unsafeHTML} from "@popeindustries/lit-html-server/directives/unsafe-html";
 
 import baseLayoutFactory from "../../client/layouts/base.js";
+
+import {loadManifest, loadSettings} from '../config.js';
 import {renderComponent} from "../engine/element-renderer";
-import {unsafeHTML} from "@popeindustries/lit-html-server/directives/unsafe-html";
 import {loadStaticProperties} from "./component-loader";
 import {loadFromCache, writeToCache} from "../cache/cache";
 import {parseMiddleware} from "../http/middleware";
 
+/**
+ * Takes a page and a layout factory and wraps the pages
+ * with the layout.
+ *
+ * Replaces the ${page} variable inside the layout.
+ *
+ * @param factory (page) => *   The layout factory which should be used
+ * @param page *                The html page fragment.
+ *
+ * @returns {Promise<string>}
+ */
 const applyLayout = async (factory, page) => {
     return renderToString(await factory(page));
 };
 
+/**
+ * Loads the page module and normalizes it so that it can be used
+ * for registering page routes.
+ *
+ * Extracts the middleware, module and page content.
+ *
+ * @param file string   The path to the page that should be loaded.
+ *
+ * @returns {Promise<{layout: *, module: *, page: *, middleware: *}>}
+ */
 const loadPageModule = async ({file}) => {
     const module = await import(path.resolve(file));
     const page = module.default;
@@ -30,6 +52,15 @@ const loadPageModule = async ({file}) => {
     }
 };
 
+/**
+ * Loads the content of an anonymous page. Anonymous pages cannot be dynamic, so
+ * they can be cached.
+ *
+ * @param module {{layout: *, module: *, page: *, middleware: *}}   The page module loaded by {@link loadPageModule}
+ * @param route string  The route where the page should be registered. Used for the cache.
+ *
+ * @returns {Promise<{markup: string, layoutFactory: *, element: *}>}
+ */
 const loadAnonymousPage = async ({ module, route = '' }) => {
     let markup = await loadFromCache(route, 'pages');
     const { page, layout } = module;
@@ -46,6 +77,16 @@ const loadAnonymousPage = async ({ module, route = '' }) => {
     };
 };
 
+/**
+ * Loads the content of a content page. Content pages can be dynamic, so we
+ * can pass the request and response objects to the page.
+ *
+ * @param module {{layout: *, module: *, page: *, middleware: *}}   The page module loaded by {@link loadPageModule}
+ * @param request *     The express request object.
+ * @param response *    The express response object.
+ *
+ * @returns {Promise<{markup: string, layoutFactory: *, element: *}>}
+ */
 const loadComponentPage = async ({ module, request, response }) => {
     const { page, layout } = module;
 
@@ -62,6 +103,18 @@ const loadComponentPage = async ({ module, request, response }) => {
     return result;
 };
 
+/**
+ * Takes a loaded page module and generates the markup. Checks if the page is an anonymous
+ * page or a component page and uses {@link loadAnonymousPage} or {@link loadComponentPage} respectively to
+ * generate the markup.
+ *
+ * @param module {{layout: *, module: *, page: *, middleware: *}}   The page module loaded by {@link loadPageModule}
+ * @param route string  The route under which the page should be registered.
+ * @param request *     The express request object
+ * @param response *    The express response object.
+ *
+ * @returns {Promise<{html: string, element: *}>}
+ */
 const generatePageMarkup = async ({module, route = '', request, response}) => {
     const result = typeof module.page?.prototype?.connectedCallback === "undefined"
         ? await loadAnonymousPage({ module, route })
@@ -78,6 +131,11 @@ const generatePageMarkup = async ({module, route = '', request, response}) => {
     }
 };
 
+/**
+ * Loads all available pages (routes) from the generated manifest.
+ *
+ * @returns {Promise<*[]>}
+ */
 const loadPages = async () => {
     const settings = await loadSettings();
 
