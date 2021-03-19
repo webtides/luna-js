51bfb9411346e04807975967e539238ae8a2626f
Lukas Schardt <lukas@buerozimmer.de>
2021-03-19 09:54:05 +0100

    #30: Added unversioned files

--- packages/luna/src/client/functions/luna.js
+++ packages/luna/src/client/functions/luna.js
@@ -1,5 +1,5 @@
 import lunaObject from "../../framework/shared/luna-object";
 
 (function() {
-    window.luna = lunaObject(window.moonConfig);
+    window.luna = lunaObject(window.lunaConfig);
 })();
