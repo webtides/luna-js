4bf80672b38258fa5a25c044f66e746ec02da1ce
Lukas Schardt <lukas@buerozimmer.de>
2021-04-02 11:10:58 +0200

    Small build cleanup, trying something with publish action

---
+++ packages/cli/build/plugins/helpers/entries.js
@@ -0,0 +1,53 @@
+const glob = require("glob-all");
+const path = require("path");
+
+const generateBasePathsFromLunaConfig = settings => {
+    const basePaths = {
+        pages: [],
+        components: [],
+        apis: [],
+        hooks: []
+    };
+
+    const pages = settings.pages.input.flatMap(page => {
+        basePaths.pages.push({
+            basePath: page
+        })
+        return glob.sync(path.join(page, "**/*.js"));
+    });
+
+    const apis = settings.api.input.flatMap(api => {
+        basePaths.apis.push({
+            basePath: api
+        });
+        return glob.sync(path.join(api, "**/*.js"));
+    });
+
+    const hooks = settings.hooks.input.flatMap(hook => {
+        basePaths.hooks.push({
+            basePath: hook
+        });
+        return glob.sync(path.join(hook, "**/*.js"));
+    });
+
+    const components = settings.components.bundles.flatMap(component => {
+        basePaths.components.push({
+            basePath: component.input,
+            settings: {
+                outputDirectory: component.output,
+            }
+        });
+        return glob.sync(path.join(component.input, "**/*.js"))
+    });
+
+    const files = [...pages, ...components, ...apis, ...hooks];
+
+    return {
+        files,
+        basePaths
+    }
+};
+
+module.exports = {
+    generateBasePathsFromLunaConfig,
+}
