b97ffee5c40ec5608019f8d65070f76d5c487398
Lukas Schardt <lukas@buerozimmer.de>
2021-04-19 08:57:25 +0200

    #47: Fixed test

--- test/tests/basic/routes.js
+++ test/tests/basic/routes.js
@@ -125,13 +125,14 @@ describe("Luna routes test", function() {
     describe("Route cache", function() {
         before(() => {
             const MemoryCache = require("../../../packages/luna/lib/framework/cache/memory-cache");
-
-            luna.set(luna.services.cache, new class extends MemoryCache {
+            class TestCache extends MemoryCache {
                 async get(key, group = 'default', defaultValue = false) {
                     await super.get(key, group, defaultValue);
                     console.log('Cache hit', { group, key });
                 }
-            })
+            }
+
+            luna.set(luna.services.cache, new TestCache());
         });
 
         it('caches the route on the second request', async function() {
