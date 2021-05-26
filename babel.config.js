0560274ecd519e84ff4f678b8e8dadab4e1f1fd2
Lukas Schardt <lukas@buerozimmer.de>
2021-05-26 08:39:22 +0200

    #46: Install specific yarn version

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = (api) => {
     api.cache(false);
 
     return {
-        presets: [["@babel/preset-env", { "targets": { "node": 12 } }]],
+        presets: [["@babel/preset-env", { "targets": { "node": 12 }, "loose": true }]],
         plugins: [
             [ "@babel/plugin-proposal-decorators", { legacy: true } ],
             [ "@babel/plugin-proposal-class-properties", { loose: true } ],
