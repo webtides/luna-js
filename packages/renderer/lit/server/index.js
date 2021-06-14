02e6c1b939bff380583fd50ed169208a9326d31b
Lukas Schardt <lukas@buerozimmer.de>
2021-06-15 00:02:23 +0200

    - Groundwork for making the render engine switchable

--- packages/renderer/lit/server/index.js
+++ packages/renderer/lit/server/index.js
@@ -1,8 +1,12 @@
 import { html, renderToString } from "@popeindustries/lit-html-server";
 import { unsafeHTML } from "@popeindustries/lit-html-server/directives/unsafe-html";
 
+const render = (template) => renderToString(template, {
+    serializePropertyAttributes: true,
+});
+
 export {
     html,
     unsafeHTML,
-    renderToString as render
+    render
 };
