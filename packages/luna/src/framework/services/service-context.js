4def79d96a5168839743559134f0ddeacd22be0b
Lukas Schardt <lukas@buerozimmer.de>
2021-07-06 09:12:06 +0200

    Updated request injection to be used with an arbitrary luna context

---
+++ packages/luna/src/framework/services/service-context.js
@@ -0,0 +1,31 @@
+/**
+ * Used for injecting services with a current request context.
+ */
+export default class ServiceContext {
+
+    constructor(context) {
+        this.context = context;
+    }
+
+    /**
+     * Returns the service instance with the current request context
+     * attached.
+     *
+     * @param service
+     * @returns {{$$luna}|*}
+     */
+    get(service) {
+        const serviceInstance = luna.get(service);
+
+        // Inject the current request into the service.
+        if (serviceInstance) {
+            serviceInstance.$$luna = {
+                ...(serviceInstance.$$luna ?? {}),
+                request: this.context?.$$luna?.request,
+                response: this.context?.$$luna?.response,
+            };
+        }
+
+        return serviceInstance;
+    }
+}
