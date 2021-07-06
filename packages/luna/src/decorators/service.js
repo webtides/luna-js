04a4867ea5368268a05b038580092f8608a59d8f
Lukas Schardt <lukas@buerozimmer.de>
2021-07-06 09:12:06 +0200

    Updated request injection to be used with an arbitrary luna context

--- packages/luna/src/decorators/service.js
+++ packages/luna/src/decorators/service.js
@@ -1,4 +1,4 @@
-import ServiceContainer from "../framework/services/service-container";
+import ServiceContext from "../framework/services/service-context";
 
 const LunaService = ({name, as, type }) => {
     return function decorator(Class) {
@@ -10,15 +10,11 @@ const LunaService = ({name, as, type }) => {
         // Set a static $$luna property to the class with meta information.
         Class.$$luna = {
             serviceName: name,
-            serviceType: type ?? LunaService.SINGLETON,
         };
 
         return Class;
     }
 };
-
-LunaService.SINGLETON = 'singleton';
-LunaService.PER_REQUEST = 'per_request';
 
 const Inject = (service) => {
     return (target, name, descriptor) => {
@@ -33,43 +29,7 @@ const Inject = (service) => {
 
         descriptor.set = function() {};
         descriptor.get = function() {
-            if (service.$$luna?.serviceType === LunaService.PER_REQUEST) {
-
-                // TODO: does a "PER_REQUEST" service make sense?
-
-                const request = this?.$$luna?.request ?? false;
-                const response = this?.$$luna?.response ?? false;
-
-                if (!request || !response) {
-                    throw new Error('Cannot use "PER_REQUEST" service on a class that isn´t in the luna lifecycle.');
-                }
-
-                const serviceName = service.$$luna.serviceName;
-
-                if (!this.$$luna.request.$$luna.services[serviceName]) {
-                    this.$$luna.request.$$luna.services[serviceName] = new service();
-                }
-
-                return this.$$luna.request.$$luna.services[serviceName];
-            }
-
-            // TODO: should an instance be set if not already present ??
-            if (!luna.get(service)) {
-                luna.set(service, new service());
-            }
-
-            const serviceInstance = luna.get(service);
-
-            // Inject the current request into the service.
-            if (serviceInstance) {
-                serviceInstance.$$luna = {
-                    ...(serviceInstance.$$luna ?? {}),
-                    request: this.$$luna?.request,
-                    response: this.$$luna?.response,
-                };
-            }
-
-            return serviceInstance;
+            return (new ServiceContext(this)).get(service);
         };
     }
 }
