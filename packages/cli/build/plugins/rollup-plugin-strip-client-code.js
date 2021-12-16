b86d8260be4e7f94ffe444619313f3106438a773
Lukas Schardt <lukas@buerozimmer.de>
2021-12-16 15:47:35 +0100

    Bugfixes that affect the new component loader and the new resolution algorithm for loading the stubs.
    Moved the luna executable to ./bin/luna.js

--- packages/cli/build/plugins/rollup-plugin-strip-client-code.js
+++ packages/cli/build/plugins/rollup-plugin-strip-client-code.js
@@ -4,6 +4,12 @@ const {requireDynamically} = require("./helpers/dynamic-require");
 
 const {getEntryType} = require("./helpers/entries");
 
+// This is used so that we can have a chain of imports
+// an still load the appropriate stub
+//
+// eg: InputElement -> BaseInputElement -> TemplateElement
+const availableEntryTypes = {};
+
 /**
  * Looks through the custom server renderers that can be defined in the luna.config.js
  * and swaps all imports on the server with the defined stubs.
@@ -20,13 +26,25 @@ module.exports = function ({ basePaths }) {
     return {
         name: 'luna-strip-client-code',
 
-        async resolveId(source, importer) {
-            const entryType = getEntryType(importer, basePaths);
+        async resolveId(source, importer, options) {
+            let entryType = getEntryType(importer, basePaths);
+
+            // This is probably pretty expensive. Is there a way with a smaller footprint?
+            const resolution = await this.resolve(source, importer, { skipSelf: true, ...options });
+
+            if (entryType === null) {
+                // There is a chance we can are a child of the entry type.
+                entryType = availableEntryTypes[importer];
+            }
 
             if (entryType && typeof entryType.settings?.factory === 'string') {
                 const { factory } = entryType.settings;
 
                 const factoryModule = requireDynamically(factory);
+
+                if (path.isAbsolute(resolution.id)) {
+                    availableEntryTypes[resolution.id] = entryType;
+                }
 
                 const stubs = await factoryModule.stubs();
                 for (const { sources, stub } of stubs) {
