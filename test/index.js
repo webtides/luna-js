cacee453d5cca737eb4e68826ca658faab1a78d2
Lukas Schardt <lukas@buerozimmer.de>
2022-01-25 15:54:51 +0100

    Added some server renderer tests for the vanilla renderer

--- test/index.js
+++ test/index.js
@@ -4,5 +4,6 @@ global.getCurrentWorkingDirectory = fixture => {
     return global.previousWorkingDirectory + "/test/fixtures/" + fixture;
 };
 
+require("./tests/unit/renderers/element-js-vanilla.js");
 require("./tests/basic.js");
 require("./tests/empty.js");
