188ca3c9eded2d2b05927d32392b467d186ee83c
Lukas Schardt <lukas@buerozimmer.de>
2022-01-29 12:16:29 +0100

    Added some base renderer tests

---
+++ test/tests/unit/renderers/base.js
@@ -0,0 +1,8 @@
+import LunaContainer from "../../../../packages/luna/src/framework/luna";
+import ComponentLoader from "../../../../packages/luna/src/framework/loaders/component-loader";
+import DocumentRenderer from "../../../../packages/luna/src/framework/engine/document-renderer";
+import ElementFactory from "../../../../packages/luna/src/framework/engine/element-factory";
+
+describe("Luna document renderer test", () => {
+
+});
