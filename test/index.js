94ee7b45ab6833d5cd71a958fd885fe651f6b0fc
Lukas Schardt <lukas@buerozimmer.de>
2022-01-30 09:42:13 +0100

    tests: Added some tests for lit renderer and service-container

--- test/index.js
+++ test/index.js
@@ -4,7 +4,11 @@ global.getCurrentWorkingDirectory = fixture => {
     return global.previousWorkingDirectory + "/test/fixtures/" + fixture;
 };
 
+require("./tests/unit/services/service-container.js");
+
 require("./tests/unit/renderers/base.js");
 require("./tests/unit/renderers/element-js-vanilla.js");
+require("./tests/unit/renderers/lit.js");
+
 require("./tests/basic.js");
 require("./tests/empty.js");
