b4687cd12e5d938885772999780a171a4b9a0f62
Lukas Schardt <hallo@lukas-schardt.de>
2022-02-11 09:26:48 +0100

    feat: Allow put/delete requests in api routes (#101)

--- packages/luna/src/framework/http/router/routes.js
+++ packages/luna/src/framework/http/router/routes.js
@@ -11,33 +11,48 @@ let currentRouter;
  * route method and registers post/get routes if available.
  *
  * @param {{router: *, route: string, middleware: *[]}}
- * @param {{ get: *, post: *}}
+ *
+ * @param getMethod
+ * @param postMethod
+ * @param putMethod
+ * @param deleteMethod
  */
-const registerRoute = ({ router, route, middleware = [] }, { get = null, post = null }) => {
-    const normalizeRoute = (method) => {
-        return (request, response) => {
+const registerRoute = ({router, route, middleware = []}, {
+        getMethod = null, postMethod = null, putMethod = null, deleteMethod = null,
+    }) => {
+        const normalizeRoute = (method) => {
+            return (request, response) => {
 
-            // Create a new service context for better dependency injection
-            // for api routes.
-            const container = new ServiceContext({
-                $$luna: {
-                    request,
-                    response
-                }
-            });
+                // Create a new service context for better dependency injection
+                // for api routes.
+                const container = new ServiceContext({
+                    $$luna: {
+                        request,
+                        response
+                    }
+                });
 
-            return method({ request, response, container });
-        }
-    };
+                return method({request, response, container});
+            }
+        };
 
-    get && (
-        router.get(route, middleware, normalizeRoute(get))
-    );
+        getMethod && (
+            router.get(route, middleware, normalizeRoute(getMethod))
+        );
 
-    post && (
-        router.post(route, middleware, normalizeRoute(post))
-    );
-};
+        postMethod && (
+            router.post(route, middleware, normalizeRoute(postMethod))
+        );
+
+        putMethod && (
+            router.put(route, middleware, normalizeRoute(putMethod))
+        );
+
+        deleteMethod && (
+            router.delete(route, middleware, normalizeRoute(deleteMethod))
+        );
+    }
+;
 
 /**
  * The main routing part of our application. All pages
@@ -57,14 +72,14 @@ const routes = async ({router}) => {
     const {apis, fallbackApi} = await luna.get(ApiLoader).loadApis();
 
     const registerPageRoute = async ({module, route}) => {
-        const callback = async ({ request, response, container }) => {
+        const callback = async ({request, response, container}) => {
             const pageMarkup = await pagesRenderer.generatePageMarkup({route, module, request, response, container});
 
             if (!pageMarkup) {
                 return response.status(404).send();
             }
 
-            const documentRenderer = new DocumentRenderer({ request, response });
+            const documentRenderer = new DocumentRenderer({request, response});
 
             const result = await documentRenderer.render(pageMarkup);
 
@@ -75,21 +90,23 @@ const routes = async ({router}) => {
             return response.send(result);
         };
 
-        registerRoute({ router, route, middleware: module.middleware }, {
-            get: callback,
-            post: callback
+        registerRoute({router, route, middleware: module.middleware}, {
+            getMethod: callback,
+            postMethod: callback
         })
 
         console.log(`Registered route ${route}`);
     }
 
     const registerApiRoute = async ({route, module}) => {
-        const { middleware } = module;
+        const {middleware} = module;
 
         const apiModule = module.module;
-        registerRoute({ router, route, middleware }, {
-            get: apiModule.get ?? apiModule.default ?? apiModule,
-            post: apiModule.post
+        registerRoute({router, route, middleware}, {
+            getMethod: apiModule.get ?? apiModule.default ?? apiModule,
+            postMethod: apiModule.post,
+            putMethod: apiModule.put,
+            deleteMethod: apiModule.remove, // This has to be `remove` because `delete` is a reserved keyword.
         });
 
         console.log(`Registered api route ${route}`);
