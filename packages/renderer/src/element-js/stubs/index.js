a8639930ecc3918eb7ca0639e573449e5489af46
Eddy Löwen <mail@eddyloewen.com>
2024-06-11 09:19:58 +0200

    chore: removes stubs from element-js renderer

--- packages/renderer/src/element-js/stubs/index.js
+++
@@ -1,5 +0,0 @@
-// import { html } from '@webtides/element-js';
-import { TemplateElement, StyledElement, BaseElement } from "../elements.js";
-const html = String.raw;
-
-export { TemplateElement, StyledElement, BaseElement, html }
