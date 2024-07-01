af0facbc7d94f4cd955438cfb6dd18e626909dda
Eddy Löwen <mail@eddyloewen.com>
2024-07-01 15:46:48 +0200

    fix: loading hooks

--- packages/luna/src/framework/loaders/hooks-loader.js
+++ packages/luna/src/framework/loaders/hooks-loader.js
@@ -16,10 +16,12 @@ class HooksLoader {
 		const manifest = await loadManifest();
 		const basePath = settings._generated.applicationDirectory;
 
-		await Promise.all(manifest.hooks.map(async ({ file }) => {
-			const module = (await import(path.resolve(path.join(basePath, file)))).default;
-			registerHook(module.name, module.default);
-		}));
+		await Promise.all(
+			manifest.hooks.map(async ({ file }) => {
+				const module = await import(path.resolve(path.join(basePath, file)));
+				registerHook(module.name, module.default);
+			}),
+		);
 	}
 }
 
